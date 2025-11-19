1510dd791b81d494eda13e12fc87bac9e84df446
guruyogendra <146312540+guruyogendra@users.noreply.github.com>
2025-11-19 13:18:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Hello from Node.js App running on Docker + Jenkins + AWS!'));
 app.listen(3000, () => console.log('App running on port 3000'));
+//hi
